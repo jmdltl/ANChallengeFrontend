2e2d0230c6c18cd3b77942bea14fe432a8b55602
Juan Miguel de la Torre Loza <migueldlt@bluetrailsoft.com>
2023-03-24 15:33:23 -0600

    Add missing placeholder

--- src/components/Login/LoginComponent.tsx
+++ src/components/Login/LoginComponent.tsx
@@ -64,6 +64,7 @@ function LoginComponent({ setToken }) {
                 Password
               </label>
               <input
+                placeholder="password"
                 type="password"
                 className="relative block w-full rounded-b-md border-0 px-3 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 {...register('password', { required: true, minLength: 8 })}
